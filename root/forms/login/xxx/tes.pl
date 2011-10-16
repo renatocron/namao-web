c531a78a65e6dd1b353cb653b948e514fdb132e8
renato cron <renato.cron@gmail.com>
2011-10-16 17:39:43 -0200

    now encodings work, you just need to remove utf8 flag on your lang.pm and disabled utf8 on ObjectUtil

--- root/forms/login/xxx/tes.pl
+++ root/forms/login/xxx/tes.pl
@@ -2,9 +2,9 @@ use YAML::XS qw /LoadFile/;
 use strict;
 
 use Data::Dumper;
- use Config::General qw(ParseConfig SaveConfig SaveConfigString);
+use Config::General qw(ParseConfig SaveConfig SaveConfigString);
 
 my $hash = LoadFile ('create.yml');
-
+print Dumper $hash;
 SaveConfig("create.conf", $hash);
 
